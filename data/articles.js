f3ebe5f881687409c61d795f6af6dfd5d604c3de
Lee Robinson <lrobinson2011@gmail.com>
2019-11-12 22:12:02 -0600

    Add Next.js vs. Gatsby vs. Create React App.

--- data/articles.js
+++ data/articles.js
@@ -1,5 +1,10 @@
 export default [
     {
+        date: 'November 12, 2019',
+        slug: 'nextjs-gatsby-create-react-app',
+        title: 'Next.js vs. Gatsby vs. Create React App'
+    },
+    {
         date: 'November 7, 2019',
         slug: 'learn-next-js',
         title: 'Why Should You Learn Next.js?'
