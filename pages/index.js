d56d39ad0172f407b68506537fc189bb0c8713b4
Lee Robinson <lrobinson2011@gmail.com>
2020-01-23 14:08:06 -0600

    Add product schema.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {CourseJsonLd} from 'next-seo';
+import {CourseJsonLd, ProductJsonLd} from 'next-seo';
 
 import Page from '../components/page';
 import WhyNextSection from '../components/sections/WhyNext';
@@ -30,6 +30,25 @@ const Index = () => (
             description="50+ lesson video course on React and Next.js."
             providerName="Lee Robinson"
             providerUrl="https://masteringnextjs.com"
+        />
+        <ProductJsonLd
+            aggregateRating={{
+                ratingValue: '5.0',
+                reviewCount: '7'
+            }}
+            description="The premiere video course for building production, server-side rendered applications with Next.js and React. Companies all over the world are using Next.js to build performant, scalable applications. This course will teach you the skills necessary to build best-in-class React applications."
+            images={['https://masteringnextjs.com/images/banner.jpg']}
+            offers={{
+                availability: 'http://schema.org/InStock',
+
+                price: '99.99',
+                priceCurrency: 'USD',
+                seller: {
+                    name: 'Lee Robinson'
+                },
+                url: 'https://masteringnextjs.com'
+            }}
+            productName="Mastering Next.js"
         />
     </>
 );
