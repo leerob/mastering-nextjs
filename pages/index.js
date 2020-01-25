df5437e40dda502184d82fbe6c50ed732560b1b7
Lee Robinson <lrobinson2011@gmail.com>
2020-01-25 15:28:35 -0600

    Added more JSON-LD info.

--- pages/index.js
+++ pages/index.js
@@ -36,19 +36,35 @@ const Index = () => (
                 ratingValue: '5.0',
                 reviewCount: '7'
             }}
+            brand="Mastering Next.js"
             description="The premiere video course for building production, server-side rendered applications with Next.js and React. Companies all over the world are using Next.js to build performant, scalable applications. This course will teach you the skills necessary to build best-in-class React applications."
             images={['https://masteringnextjs.com/images/banner.jpg']}
             offers={{
                 availability: 'http://schema.org/InStock',
-
+                itemCondition: 'http://schema.org/NewCondition',
                 price: '99.99',
                 priceCurrency: 'USD',
+                priceValidUntil: '2021-02-12',
                 seller: {
                     name: 'Lee Robinson'
                 },
                 url: 'https://masteringnextjs.com'
             }}
             productName="Mastering Next.js"
+            reviews={[
+                {
+                    author: 'Matt DeJong',
+                    datePublished: '2020-01-24T03:37:40Z',
+                    name: 'Shipped my first Next.js app last week - thank you!',
+                    reviewBody:
+                        'Hands down the best way to learn Next.js. High quality videos & audio. Would definitely recommend to others wanting to learn Next.js and React.',
+                    reviewRating: {
+                        bestRating: '5',
+                        ratingValue: '5',
+                        worstRating: '1'
+                    }
+                }
+            ]}
         />
     </>
 );
