78611a30da726eab6bad2cbe3afcb2e3a48df0c5
Lee Robinson <lrobinson2011@gmail.com>
2020-01-26 20:44:04 -0600

    Update colors.

--- components/sections/Payment.js
+++ components/sections/Payment.js
@@ -7,9 +7,8 @@ const Section = styled.section`
     display: flex;
     align-items: center;
     justify-content: center;
-    background: linear-gradient(217deg, rgba(255, 0, 0, 0.42), rgba(255, 0, 0, 0) 70.71%),
-        linear-gradient(127deg, rgba(0, 255, 0, 0.43), rgba(0, 255, 0, 0) 70.71%),
-        linear-gradient(336deg, rgba(0, 0, 255, 0.45), rgba(0, 0, 255, 0) 70.71%);
+    background: linear-gradient(336deg, rgba(0, 0, 255, 0.45), rgba(0, 0, 255, 0) 70.71%),
+        linear-gradient(217deg, rgba(255, 0, 0, 0.42), rgba(255, 0, 0, 0) 70.71%);
 `;
 
 const Card = styled.div`
