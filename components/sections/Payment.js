a33f4a9d8e9fc18fe69ba51f0741ab23fa6bf611
Lee Robinson <lrobinson2011@gmail.com>
2020-01-26 20:47:02 -0600

    Make the dollar sign smaller.

--- components/sections/Payment.js
+++ components/sections/Payment.js
@@ -59,6 +59,13 @@ const Price = styled.p`
     font-size: 60px;
     color: black;
     margin: 8px;
+    display: flex;
+    align-items: center;
+
+    small {
+        margin-right: 2px;
+        font-size: 32px;
+    }
 `;
 
 const Currency = styled.p`
@@ -107,7 +114,10 @@ const PaymentSection = () => (
             </CardHeader>
             <PriceContainer>
                 <OriginalPrice>{'$199'}</OriginalPrice>
-                <Price>{'$99'}</Price>
+                <Price>
+                    <small>{'$'}</small>
+                    {'99'}
+                </Price>
                 <Currency>{'USD'}</Currency>
             </PriceContainer>
             <Features>
