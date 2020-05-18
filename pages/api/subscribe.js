d4823115ff7174a695e515bbe1984358e37dc9e9
Lee Robinson <lrobinson@hy-vee.com>
2020-05-18 07:20:57 -0500

    Improve email error message.

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -1,34 +1,35 @@
-import fetch from 'isomorphic-unfetch';
-
 export default async (req, res) => {
-    const {email} = req.body;
+  const { email } = req.body;
 
-    if (!email) {
-        return res.status(400).json({error: 'Email is required'});
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required' });
+  }
+
+  try {
+    const API_KEY = process.env.BUTTONDOWN_API_KEY;
+    const response = await fetch(
+      `https://api.buttondown.email/v1/subscribers`,
+      {
+        body: JSON.stringify({
+          email,
+          tags: ['Mastering Next.js']
+        }),
+        headers: {
+          Authorization: `Token ${API_KEY}`,
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }
+    );
+
+    if (response.status >= 400) {
+      return res.status(400).json({
+        error: await response.text()
+      });
     }
 
-    try {
-        const API_KEY = process.env.BUTTONDOWN_API_KEY;
-        const response = await fetch(`https://api.buttondown.email/v1/subscribers`, {
-            body: JSON.stringify({
-                email,
-                tags: ['Mastering Next.js'],
-            }),
-            headers: {
-                Authorization: `Token ${API_KEY}`,
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-        });
-
-        if (response.status >= 400) {
-            return res.status(400).json({
-                error: `There was an error signing up. Shoot me an email at [me@leerob.io] and I'll add you to the list.`,
-            });
-        }
-
-        return res.status(201).json({error: ''});
-    } catch (error) {
-        return res.status(500).json({error: error.message || error.toString()});
-    }
+    return res.status(201).json({ error: '' });
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() });
+  }
 };
